9bf25f49d72eb7dcefa67168205d8245854eca45
Keith Baker <github@baker.is>
2024-05-03 01:04:04 -0400

    Writing Useful Tests

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -2,6 +2,12 @@ package main
 
 import "testing"
 
-func TestName(t *testing.T) {
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
 
+	if len(d) != 52 {
+		t.Errorf("NewDeck returned wrong number of cards, expected 52 got %d", len(d))
+	}
 }
+
+func TestSaveToDeckandNewDeckFromFile(t *testing.T) {}
