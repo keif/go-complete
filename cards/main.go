6cb84371d143426f7377555750c15e3b48c4f1db
Keith Baker <github@baker.is>
2024-05-02 23:47:24 -0400

    functions and return types

--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades"
-	// go compiler will figure out the above is the above
-	// := says "we have a variable, infer the type based on the variable"
-	// we only use := on DEFINITION not reassignment
-	card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
