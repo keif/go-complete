dca5f9afedc94fdfdfa7c0d5e4caafc664256199
Keith Baker <github@baker.is>
2024-05-02 23:43:14 -0400

    variable declarations

--- cards/main.go
+++ cards/main.go
@@ -1,4 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
+	// var card string = "Ace of Spades"
+	// go compiler will figure out the above is the above
+	// := says "we have a variable, infer the type based on the variable"
+	// we only use := on DEFINITION not reassignment
+	card := "Ace of Spades"
+
+	fmt.Println(card)
 }
