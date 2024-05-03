f69ffbdd9cb7ef69cc31852ea6fe5a9dfd91a6a6
Keith Baker <github@baker.is>
2024-05-03 01:10:40 -0400

    Testing File I/O

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -18,4 +21,23 @@ func TestNewDeck(t *testing.T) {
 	}
 }
 
-func TestSaveToDeckandNewDeckFromFile(t *testing.T) {}
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck, _ := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 52 {
+		t.Errorf("NewDeck returned wrong number of cards, expected 52 got %d", len(loadedDeck))
+	}
+
+	if loadedDeck[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, got %s", loadedDeck[0])
+	}
+
+	if loadedDeck[len(loadedDeck)-1] != "King of Clubs" {
+		t.Errorf("Expected first card of Ace of Spades, got %s", loadedDeck[len(loadedDeck)-1])
+	}
+}
