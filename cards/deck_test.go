a945bf7d75cdb1f079a541eb740f2041098624c7
Keith Baker <github@baker.is>
2024-05-03 01:05:56 -0400

    Asserting Elements in a Slice

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -8,6 +8,14 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("NewDeck returned wrong number of cards, expected 52 got %d", len(d))
 	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, got %s", d[0])
+	}
+
+	if d[len(d)-1] != "King of Clubs" {
+		t.Errorf("Expected first card of Ace of Spades, got %s", d[len(d)-1])
+	}
 }
 
 func TestSaveToDeckandNewDeckFromFile(t *testing.T) {}
