0c37a14c2737cdc623d293f5bf7bc117111f18c2
Keith Baker <github@baker.is>
2024-05-04 11:13:46 -0400

    Manipulating maps

--- map/main.go
+++ map/main.go
@@ -3,11 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#00ff00",
-		"blue":  "#0000ff",
-	}
+	//var colors map[string]string
+
+	//colors := make(map[string]string)
+	//colors["white"] = "#ffffff"
+	//fmt.Println(colors["white"])
+
+	colors := make(map[int]string)
+	colors[10] = "#ffffff"
+
+	delete(colors, 10)
+
+	//colors := map[string]string{
+	//	"red":   "#ff0000",
+	//	"green": "#00ff00",
+	//	"blue":  "#0000ff",
+	//}
 
 	fmt.Println(colors)
 }
