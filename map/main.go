2a319abf2e3b25e7f7f28d6d391d3c7a4c9f782a
Keith Baker <github@baker.is>
2024-05-04 11:21:31 -0400

    Differences between maps and structs

--- map/main.go
+++ map/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func main() {
+	// maps are great for dynamic additions, like adding a new color
+	// we want them to be tightly coupled, i.e. "these are all colors"
+	// a struct is more tightly coupled in the "if we only work with the initially created key/value pairs"
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#00ff00",
@@ -10,13 +13,15 @@ func main() {
 		"white": "#ffffff",
 	}
 
+	colors["yellow"] = "#123456"
+	delete(colors, "yellow")
+
 	//fmt.Println(colors)
 	printMap(colors)
 }
 
 func printMap(c map[string]string) {
 	for color, hex := range c {
-		fmt.Println(color, hex)
 		fmt.Printf("Hex code for %c is %s\n", color, hex)
 	}
 }
