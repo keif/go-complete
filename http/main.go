e380d7832070d3f0a649df864e1702365aef1d58
Keith Baker <github@baker.is>
2024-05-06 14:46:32 -0400

    Working with the Read Function

--- http/main.go
+++ http/main.go
@@ -15,5 +15,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(resp)
+	bs := make([]byte, 999999)
+	//bs := make([]byte, resp.ContentLength)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+
+	//bytesRead, err := resp.Body.Read(bs)
+	//if err != nil {
+	//	fmt.Println("Error:", err)
+	//	os.Exit(1)
+	//}
+	//fmt.Println(string(bs[:bytesRead]))
+
 }
