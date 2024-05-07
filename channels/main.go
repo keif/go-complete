34fbc9ec7ea14a69e35f4e79de1903450d5fe83a
Keith Baker <github@baker.is>
2024-05-07 13:00:46 -0400

    Blocking Channels

--- channels/main.go
+++ channels/main.go
@@ -27,6 +27,12 @@ func main() {
 	}
 
 	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
 
 func checkLink(link string, c chan string) bool {
