deaaabc6e01d57cb517ec6fd85ac363fec8247b3
Keith Baker <github@baker.is>
2024-05-07 12:27:44 -0400

    section-6 read file assignment answer

---
+++ read-assignment/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// check that we have an argument to work with
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <filename>")
+		os.Exit(1)
+	}
+
+	// get the filename from the CLI arguments
+	fileName := os.Args[1]
+
+	// open the file
+	file, err := os.Open(fileName)
+	if err != nil {
+		fmt.Printf("Error opening file: %v\n", err)
+	}
+	defer file.Close() // close file when done
+
+	// read the file
+	content, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	// print file content
+	fmt.Println(string(content))
+}
